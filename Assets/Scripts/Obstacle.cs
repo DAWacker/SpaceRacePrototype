d47b6d393b065a8c91b80189045b24f26e38b7e6
David Wilson <daw4475@g.rit.edu>
2015-04-02 02:06:24 -0400

    obstalces spawn

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public bool IsActive { get; set; }
+
+    void Start()
+    {
+        IsActive = false;
+    }
+
+    void FixedUpdate()
+    {
+        // Move the platform towards the camera every fixed frame
+        if (IsActive)
+        {
+            transform.Translate(0.0f, 0.0f, -Constants.WORLD_SPEED * Time.deltaTime);
+
+            Vector3 pos = transform.position;
+            if (pos.z <= Constants.RECYCLE_POINT)
+            {
+                transform.position = new Vector3(0.0f, 0.5f, -10.0f);
+                IsActive = false;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class ObstacleManager : MonoBehaviour
+{
+    List<Obstacle> obstacles;
+    float[] locations = new float[] { -3.0f, 0.0f, 3.0f };
+    System.Random rand;
+
+    float timeSinceLastSpawn;
+    float spawnDelay;
+
+    void Start()
+    {
+        // Gather all platforms in the scene
+        obstacles = FindObjectsOfType<Obstacle>().ToList();
+
+        rand = new System.Random();
+
+        timeSinceLastSpawn = 0.0f;
+        spawnDelay = 0.25f;
+    }
+
+    void SpawnObstacle()
+    {
+        Obstacle obstacle = obstacles.FirstOrDefault(o => !o.IsActive);
+
+        if (obstacle)
+        {
+            float newLocationX = locations[rand.Next(0, 3)];
+
+            if (obstacle)
+            {
+                obstacle.transform.position = new Vector3(newLocationX, 0.5f, 20.0f);
+                obstacle.IsActive = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= spawnDelay)
+        {
+            timeSinceLastSpawn = 0.0f;
+            SpawnObstacle();
+        }
+    }
+}
